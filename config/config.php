1032c6ad07a1b1214316b1dace9e9f5963f8bce4
renaiku <corbeau.lenaic@gmail.com>
2017-10-23 21:21:04 +0200

    add constant SERVER_ADRESS

--- config/config.php
+++ config/config.php
@@ -6,6 +6,8 @@
 	define('DB_USER',		'root');
 	define('DB_PASSWORD',	'');
 
+	define('SERVER_ADDRESS',	'localhost');
+
 	define('DOMAIN_NAME',	'localhost/git/esx_webadmin/');
 	define('STEAM_API_KEY',	'97C1C2B4592752472E52E35C4CA8311E');
 	define('USE_WHITELIST',	true);
