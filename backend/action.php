7c0ee45ee7cd1ba134c2f3d7f0e2c6674c80c285
renaiku <corbeau.lenaic@gmail.com>
2017-11-16 20:10:55 +0100

    add more rcon commands

--- backend/action.php
+++ backend/action.php
@@ -1,32 +1,33 @@
 <?php
 
 require("rcon.php");
+include("../config/config.php");
 
-if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-    $user_id = $_GET['uid'];
-    $server_id = $_GET['sid'];
+$con = new q3query(GAME_SERVER_ADDRESS, GAME_SERVER_PORT, $success);
+
+if (!$success) {
+    die ("blah");
 }
+
+$con->setRconpassword(GAME_SERVER_RCON_PASSWORD);
         
-// Check for action in url, imports the variables and do command.    
-if($action == "kick") {
-       
-    foreach ($serverinfo as $server) {
-        if($server['0'] == $server_id){
-            
-            $con = new q3query($server['2'], $server['3'], $success);
-
-            if (!$success) {
-                die ("blah");
-            }
-            
-            $con->setRconpassword($server['4']);
-            $con->rcon("clientkick $user_id $kickmessage");
-            echo "You successfully should have kicked the user with ID $user_id. Redirect after 3 seconds.";
-            header( "refresh:3;url=index.php" );
-            die();
-        }        
-    }
+  
+if($_GET['action'] == "kick") {
+    $con->rcon("clientkick " . $_GET['id'] . " " . $_GET['msg']);
+    echo "You successfully should have kicked the user ". $_GET['id'] . ". Redirect after 3 seconds.";
 }
+
+if($_GET['action'] == "say") {
+    $con->rcon("say " . $_GET['msg']);
+    echo "You successfully sent a message to the in-game chat. Redirect after 3 seconds.";
+}
+
+if($_GET['action'] == "setjob") {
+    $con->rcon("setjob " . $_GET['id'] . " " . $_GET['job'] . " " . $_GET['grade']);
+    echo "You successfully changed a job. Redirect after 3 seconds.";
+}
+
+//header( "refresh:3;url=index.php" );
+//die();
 
 ?>
--- config/config.php
+++ config/config.php
@@ -9,11 +9,16 @@
 	define('DB_USER',		'root');
 	define('DB_PASSWORD',	'1202');
 
-	// SERVER CONFIG
+	// WEBSITE SERVER CONFIG
 	define('SERVER_ADDRESS',	'localhost');
 	define('SERVER_NAME',		'My Server');
-	define('DOMAIN_NAME',	'localhost/esx_webadmin/');
-	define('STEAM_API_KEY',	'97C1C2B4592752472E52E35C4CA8311E');
-	define('USE_WHITELIST',	true);
+	define('DOMAIN_NAME',		'localhost/esx_webadmin/');
+	define('STEAM_API_KEY',		'97C1C2B4592752472E52E35C4CA8311E');
+	define('USE_WHITELIST',		true);
+
+	// GAME SERVER CONFIG
+	define('GAME_SERVER_ADDRESS',		'127.0.0.1');
+	define('GAME_SERVER_PORT',			'30120');
+	define('GAME_SERVER_RCON_PASSWORD',	'fxmonanus');
 
 ?>
