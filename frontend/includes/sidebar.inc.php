51ed2c221c826146c6a2c4aa06221e7acbe82822
renaiku <corbeau.lenaic@gmail.com>
2017-11-16 15:15:11 +0100

    Cleaning useless options

--- frontend/includes/sidebar.inc.php
+++ frontend/includes/sidebar.inc.php
@@ -26,43 +26,29 @@
 	  <!-- sidebar menu: : style can be found in sidebar.less -->
 	  <ul class="sidebar-menu" data-widget="tree">
 	    <li class="header"><?php echo SERVER_NAME; ?></li>
-	    <li class="active treeview">
-	      <a href="#">
-	        <i class="fa fa-dashboard"></i> <span>Home</span>
-	        <span class="pull-right-container">
-	          <i class="fa fa-angle-left pull-right"></i>
-	        </span>
-	      </a>
-	    </li>
+	    <!-- ADMIN -->
 	    <?php
 		    if($user['group'] == 'superadmin') {
 			    echo '<li class="treeview">
 			      <a href="#">
-			        <i class="fa fa-files-o"></i>
-			        <span>Administration</span>
+			        <i class="fa fa-files-o"></i> <span>Administration</span>			        
 			        <span class="pull-right-container">
-			          <span class="label label-primary pull-right">4</span>
+			          <i class="fa fa-angle-left pull-right"></i>
 			        </span>
 			      </a>
 			      <ul class="treeview-menu">
-			        <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Whitelist</a></li>
-			        <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-			        <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-			        <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+			        <li><a href="?p=whitelist"><i class="fa fa-circle-o"></i> Whitelist</a></li>
+			        <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Settings</a></li>
 			      </ul>
 			    </li>';
 		    }
 	    ?>
-
+	    <!-- SOCIETY BOSS -->
 	    <?php
 		    if($user['job_grade_name'] == 'boss') {
 			    echo '<li class="treeview">
 			      <a href="#">
-			        <i class="fa fa-files-o"></i>
-			        <span>My Society</span>
-			        <span class="pull-right-container">
-			          <span class="label label-primary pull-right">4</span>
-			        </span>
+			        <i class="fa fa-files-o"></i> <span>My Society</span>
 			      </a>
 			      <ul class="treeview-menu">
 			        <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Whitelist</a></li>
@@ -78,45 +64,9 @@
 	      <a href="#">
 	        <i class="fa fa-files-o"></i>
 	        <span>My Character</span>
-	        <span class="pull-right-container">
-	          <span class="label label-primary pull-right">4</span>
-	        </span>
 	      </a>
 	    </li>
 
-	    <li class="treeview">
-	      <a href="#">
-	        <i class="fa fa-share"></i> <span>Multilevel</span>
-	        <span class="pull-right-container">
-	          <i class="fa fa-angle-left pull-right"></i>
-	        </span>
-	      </a>
-	      <ul class="treeview-menu">
-	        <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-	        <li class="treeview">
-	          <a href="#"><i class="fa fa-circle-o"></i> Level One
-	            <span class="pull-right-container">
-	              <i class="fa fa-angle-left pull-right"></i>
-	            </span>
-	          </a>
-	          <ul class="treeview-menu">
-	            <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-	            <li class="treeview">
-	              <a href="#"><i class="fa fa-circle-o"></i> Level Two
-	                <span class="pull-right-container">
-	                  <i class="fa fa-angle-left pull-right"></i>
-	                </span>
-	              </a>
-	              <ul class="treeview-menu">
-	                <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-	                <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-	              </ul>
-	            </li>
-	          </ul>
-	        </li>
-	        <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-	      </ul>
-	    </li>
 	    <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
 	  </ul>
 	</section>
